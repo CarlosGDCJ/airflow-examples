113aee3f2afc10539100ec3f66714c601675d41e
Carlos <carlosgdcj@hotmail.com>
2023-06-01 22:37:20 -0300

    Fixed planets page, add db operator

--- wikipedia/dags/main.py
+++ wikipedia/dags/main.py
@@ -1,28 +1,31 @@
-import datetime as dt
-import pandas as pd
+import pendulum
 from urllib.request import urlretrieve
 
 from airflow.operators.bash import BashOperator
 from airflow.operators.python import PythonOperator
+from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow import DAG
 
 
-today = dt.datetime.today()
+today = pendulum.today("utc")
 dag = DAG(
     dag_id="wikipedia_pageviews",
     start_date=today,
     schedule_interval="@hourly",
+    template_searchpath="/tmp",
 )
 
 
-def _get_data(data_interval_start):
-    year, month, day, hour, *_ = data_interval_start.timetuple()
+def _get_data(logical_date):
+    year, month, day, hour, *_ = logical_date.timetuple()
     url = (
-        "'https://dumps.wikimedia.org/other/pageviews/"
+        "https://dumps.wikimedia.org/other/pageviews/"
         f"{year}/{year}-{month:02}/"
         f"pageviews-{year}{month:02}{day:02}-"
-        f"{hour:02}0000.gz'"
+        f"{hour:02}0000.gz"
     )
+    print("Retrieving URL:")
+    print("", url, sep="\t")
 
     output_path = "/tmp/pageviews.gz"
     urlretrieve(url, output_path)
@@ -38,24 +41,33 @@ get_data = PythonOperator(
 
 extract_pageview = BashOperator(
     task_id="extract_pageview",
-    bash_command="gzip -d /tmp/pageview.gz",
+    bash_command="gzip -df /tmp/pageviews.gz",
     dag=dag,
 )
 
 
-def _process_data(pagenames, language):
-    freqs = dict.fromkeys(pagenames, 0)
+def _process_data(pagenames, language, logical_date):
+    pageviews = dict.fromkeys(pagenames, 0)
     with open("/tmp/pageviews", "r") as f:
         for line in f:
             domain_code, page_title, view_count, *_ = line.split(" ")
-            if domain_code == language and page_title in freqs:
-                freqs[page_title] = view_count
+            if domain_code == language and page_title in pagenames:
+                pageviews[page_title] = view_count
 
-    print(freqs)
+    with open("/tmp/postgres_query.sql", "w") as f:
+        for pagename, view_count in pageviews.items():
+            f.write(
+                "INSERT INTO pageview_counts VALUES ("
+                f"'{pagename}, {view_count}, {logical_date}"
+                ");\n"
+            )
+
+    print(pageviews)
 
 
-solar_system = (
-    "Mercury",
+solar_system = {
+    "Sun",
+    "Mercury_(planet)",
     "Venus",
     "Earth",
     "Mars",
@@ -63,11 +75,19 @@ solar_system = (
     "Saturn",
     "Uranus",
     "Neptune",
-)
+}
 process_data = PythonOperator(
     task_id="process_data",
     python_callable=_process_data,
     op_kwargs={"pagenames": solar_system, "language": "en"},
+    dag=dag,
+)
+
+
+write_to_postgres = PostgresOperator(
+    task_id="write_to_postgres",
+    postgres_conn_id="my_postgres",
+    sql="postgres_query.sql",
     dag=dag,
 )
 
